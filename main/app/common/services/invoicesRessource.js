f57e0feb11d25939d3ea098424f14418399706c3
lds <lds@itesoft.com>
2016-02-10 13:03:31 +0100

    Update 2016-02-10T12:02:26.738Z

--- main/app/common/services/invoicesRessource.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('BalanceForms').factory('Invoices', ['$resource', function ($resources) {
-    return $resources('http://clilds:8080/api/rest/get', {},
-        {
-            getAll: {method:'GET', params:{}, isArray:true}
-        }
-    );
-}])
